cac3a9565a537beac0e6e5a7a74d63af2222641c
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-14 14:12:23 -0500

    basic functionality all there, needs styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    doggos: [],
+    doggoText: ''
+  }
+
+  componentDidMount(){
+    axios.get('https://dog.ceo/api/breed/husky/images')
+    .then(res => {
+      this.setState({
+        doggos: res.data.message
+      })
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.doggos !== prevState.doggos){
+      if (this.state.doggoText === 'chihuahua'){
+        axios
+        .get('https://dog.ceo/api/breed/husky/images')
+        .then(res => {
+          this.setState({
+            doggos: res.data.message,
+            doggoText: 'husky'
+          })
+        })
+        .catch(err => console.log(err))
+      }
+    }
+  }
+
+  handleChanges = e => {
+    this.setState({
+      doggoText: e.target.value
+    })
+  }
+
+  fetchDoggos = e => {
+    e.preventDefault()
+    axios
+    .get(`https://dog.ceo/api/breed/${this.state.doggoText}/images`)
+    .then(res => {
+      this.setState({
+        doggos: res.data.message
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  render(){
+    return (  
+     <div className='App'>
+       <h1>Doggos</h1>
+       <input 
+        type='text'
+        value={this.state.doggoText}
+        onChange={this.handleChanges} 
+      />
+      <button onClick={this.fetchDoggos}>Fetch Doggos</button>
+      <div className='doggos'>
+        {this.state.doggos.map(doggo => (
+          <img width='200' src={doggo} alt={doggo} />
+        ))}
+      </div>
+     </div>
+    );
+  }
 }
 
 export default App;
